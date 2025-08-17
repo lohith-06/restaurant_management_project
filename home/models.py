fab7f247b5cfac1669ecccf63d42c466d71a09b3
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-17 12:59:53 +0530

    32372

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback {self.id} - {self.created_at.strftime('%Y-%m-%d')}"
