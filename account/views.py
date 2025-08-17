24269b3fc1bffa33b094cb26043f710ca1dda831
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-17 13:00:19 +0530

    32372

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
+
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_thankyou')  # after submit redirect to thank you page
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
+
+def feedback_thankyou(request):
+    return render(request, 'thankyou.html')
