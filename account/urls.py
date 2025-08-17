e8960546159a5d7079e72dcaa868a17bf3cff0af
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-17 13:00:49 +0530

    32372

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('feedback/', views.feedback_view, name='feedback'),
+    path('feedback/thankyou/', views.feedback_thankyou, name='feedback_thankyou'),
 ]
