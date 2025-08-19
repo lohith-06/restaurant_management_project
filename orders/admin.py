cc535d4439ad4cb9140cb46f38de106fcd30ecbc
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-19 23:22:49 +0530

    34477

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Menu, Order, OrderItem
+
+# Inline to show items inside an order
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "available")
+    search_fields = ("name",)
+    list_filter = ("available",)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "status", "total_amount", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("customer__username", "id")
+    inlines = [OrderItemInline]
