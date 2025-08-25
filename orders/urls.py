8397f5b10e08948762fc24702db120bcc6a81446
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-25 18:09:16 +0530

    38290

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import get_menu
+from . import views
 
 urlpatterns = [
     path("api/menu/", get_menu, name="get_menu"),
+    path("contact/", views.contact_us, name="contact"),
+    path("contact/success/", views.contact_us, name="contact_success"),
 ]
