0dcb1349e99005f94a19c3863cb937b3e93f4bd1
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-25 18:16:34 +0530

    38291

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "created_at")   # columns in admin list view
+    search_fields = ("name", "email")               # search bar
+    list_filter = ("created_at",) 
