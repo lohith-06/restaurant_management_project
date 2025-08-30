706407b5ae000865e9aefd22846d341319d7f778
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-31 00:04:43 +0530

    39923

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 # Create your models here.
+
 from django.db import models
 
-class Feedback(models.Model):
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
 
     def __str__(self):
-        return f"Feedback {self.id} - {self.created_at.strftime('%Y-%m-%d')}"
+        return f"{self.name} ({self.email})"k {self.id} - {self.created_at.strftime('%Y-%m-%d')}"
