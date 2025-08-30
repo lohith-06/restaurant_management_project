552a1b05344dd8f195f2f8f903a9e0e3a9a979d3
lohith-06 <lohithlakkakula06@gmail.com>
2025-08-31 00:05:51 +0530

    39922

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
 # Create your models here.
-
+# models.py
 from django.db import models
 
-class Contact(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.name} ({self.email})"k {self.id} - {self.created_at.strftime('%Y-%m-%d')}"
+        return self.name
