fe0da1aa9bb310f23332ef2ba2cd10c5433bb5c1
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-02 11:50:01 +0530

    41218

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
+# from django.shortcuts import render
+# from django.conf import settings
+
+# def home_view(request):
+#     context = {
+#         'restaurant_name': settings.RESTAURANT_NAME
+#     }
+#     return render(request, 'home/index.html', context)
+
+# def about_view(request):
+#     return render(request, 'home/about.html')# views.py
 from django.shortcuts import render
-from django.conf import settings
+from .models import RestaurantLocation
 
-def home_view(request):
-    context = {
-        'restaurant_name': settings.RESTAURANT_NAME
-    }
-    return render(request, 'home/index.html', context)
-
-def about_view(request):
-    return render(request, 'home/about.html')
+def home(request):
+    location = RestaurantLocation.objects.first()  # get first saved location
+    return render(request, "home.html", {"location": location})
