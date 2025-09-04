10203e9cac4ba3f1f7191f563c89ab1989ecdd94
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-04 21:23:17 +0530

    41789

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name='home'),
-path("menu/", views.menu_view, name="menu"),
+path("menu/", views.menu_view, name="menu"),   
+ path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'), 
 ]
