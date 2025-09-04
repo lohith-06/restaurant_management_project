58c99f553b3b4538daaa28957ec4e7aee6be9ef7
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-04 21:17:49 +0530

    41791

--- home/models.py
+++ home/models.py
@@ -31,13 +31,25 @@
 
 #     def __str__(self):
 #         return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
+# from django.db import models
+
+# class MenuItem(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # New field
+
+#     def __str__(self):
+#         return self.name
 from django.db import models
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # New field
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)  # ✅ new field
 
     def __str__(self):
         return self.name
