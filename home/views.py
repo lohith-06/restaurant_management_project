0191e5a881d666ee8cfd8f7813b3e63a151e51c2
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-06 23:31:14 +0530

    42850

--- home/views.py
+++ home/views.py
@@ -15,22 +15,36 @@
 # def home(request):
 #     location = RestaurantLocation.objects.first()  # get first saved location
 #     return render(request, "home.html", {"location": location})# views.py
+# from django.shortcuts import render
+# from .models import MenuItem, RestaurantLocation
+
+# def home(request):
+#     query = request.GET.get("q")  # search keyword
+#     if query:
+#         menu_items = MenuItem.objects.filter(name__icontains=query)
+#     else:
+#         menu_items = MenuItem.objects.all()
+
+#     location = RestaurantLocation.objects.first()
+
+#     return render(
+#         request,
+#         "home.html",
+#         {"menu_items": menu_items, "location": location, "query": query},
+# def home_view(request):
+#     restaurant = Restaurant.objects.first()  # assuming one restaurant
+#     return render(request, "home.html", {"restaurant": restaurant})
 from django.shortcuts import render
-from .models import MenuItem, RestaurantLocation
+from django.utils import timezone
+from .models import Restaurant
+from .utils import get_cart_count
 
-def home(request):
-    query = request.GET.get("q")  # search keyword
-    if query:
-        menu_items = MenuItem.objects.filter(name__icontains=query)
-    else:
-        menu_items = MenuItem.objects.all()
-
-    location = RestaurantLocation.objects.first()
-
-    return render(
-        request,
-        "home.html",
-        {"menu_items": menu_items, "location": location, "query": query},
 def home_view(request):
-    restaurant = Restaurant.objects.first()  # assuming one restaurant
-    return render(request, "home.html", {"restaurant": restaurant})
+    restaurant = Restaurant.objects.first()
+    cart_count = get_cart_count(request)
+    current_time = timezone.now()
+    return render(request, "home.html", {
+        "restaurant": restaurant,
+        "cart_count": cart_count,
+        "current_time": current_time
+    })
