82eb16985634ce2a32b071ef9e70dab41841d840
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-06 23:34:38 +0530

    42851

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', home_view, name='home'),
 path("menu/", views.menu_view, name="menu"),   
  path('', views.home_view, name='home'),
+    path('faq/', views.faq_view, name='faq'),
     path('about/', views.about_view, name='about'), 
 ]
