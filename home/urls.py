6d3fb47f52062b1bea849b7c00624b8ef1ac8bb5
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-10 22:47:26 +0530

    43333

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path("", views.home_view, name="home"),  
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 path("menu/", views.menu_view, name="menu"),   
  path('', views.home_view, name='home'),
     path('faq/', views.faq_view, name='faq'),
