1f37019563a941d8fb7cd7fe0857c4d568f4a145
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-12 21:15:15 +0530

    43946

--- home/models.py
+++ home/models.py
@@ -74,3 +74,10 @@ class TodaySpecial(models.Model):
 
     def __str__(self):
         return self.item_name
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
