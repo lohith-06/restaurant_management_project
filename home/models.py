25a8ac7d38fc2fa80a515f86dc498e15a110e3d3
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-12 21:11:48 +0530

    43944

--- home/models.py
+++ home/models.py
@@ -66,3 +66,11 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class TodaySpecial(models.Model):
+    item_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.item_name
