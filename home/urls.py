af308460b596dfcc2772ff93c6f3a40671d93cbd
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-15 22:26:46 +0530

    44596

--- home/urls.py
+++ home/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [\
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 path("menu/", views.menu_view, name="menu"),   
- path('', views.home_view, name='home'),
+ path('', views.home_view, name='home'), path('thank-you/', views.thank_you, name='thank_you'),
     path('faq/', views.faq_view, name='faq'), path('contact/', views.contact_us, name='contact_us'),
     path('about/', views.about_view, name='about'), 
     path('privacy-policy/', views.privacy_policy_view, name='privacy_policy'),path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
