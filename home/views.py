c86a13b0ebf40640676cbfe4db0458c952189211
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-15 22:29:39 +0530

    44597

--- home/views.py
+++ home/views.py
@@ -56,7 +56,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import MenuItem, RestaurantInfo # assume you already have this model
+from .forms import ContactForm
 
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # handle valid data
+            return redirect('thank_you')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
 def contact_us(request): 
     if request.method == 'POST':
         name = request.POST.get('name')
