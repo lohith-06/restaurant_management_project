1a6f59ac70dcfed2ee96c0dcc37b33405a3488d5
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-15 22:23:26 +0530

    44595

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Contact
+from .models import RestaurantInfo
 
+admin.site.register(RestaurantInfo)
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "created_at")   # columns in admin list view
