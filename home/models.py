20dece3691a539fa86e7f2700daf7cd28c9deeac
lohith-06 <lohithlakkakula06@gmail.com>
2025-09-22 22:27:39 +0530

    48074

--- home/models.py
+++ home/models.py
@@ -1,26 +1,26 @@
 # Create your models here.
 # models.py
-# from django.db import models
+from django.db import models
 
-# class MenuItem(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
-#     def __str__(self):
-#         return self.name# models.py
-# models.py
-# from django.db import models
+    def __str__(self):
+        return self.name# models.py
+models.py
+from django.db import models
 
-# class MenuItem(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = models.ImageField(upload_to="menu_images/", blank=True, null=True)  # new field
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)  # new field
 
-#     def __str__(self):
-#         return self.name# models.py
+    def __str__(self):
+        return self.name# models.py
 # from django.db import models
 
 # class RestaurantLocation(models.Model):
@@ -53,19 +53,19 @@
 
 #     def __str__(self):
 #         return self.name
-from django.db import models
+# from django.db import models
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.TextField()
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=10)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    logo = models.ImageField(upload_to="restaurant_logos/", blank=True, null=True)  # ✅ new field
+# class Restaurant(models.Model):
+#     name = models.CharField(max_length=200)
+#     address = models.TextField()
+#     city = models.CharField(max_length=100)
+#     state = models.CharField(max_length=100)
+#     zip_code = models.CharField(max_length=10)
+#     phone_number = models.CharField(max_length=15, blank=True, null=True)
+#     logo = models.ImageField(upload_to="restaurant_logos/", blank=True, null=True)  # ✅ new field
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
         
 class OpeningHour(models.Model):
     day = models.CharField(max_length=20)
