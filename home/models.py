e44aa598a56ed198ba805e9d80c9d3c225a763e4
lohith-06 <lohithlakkakula06@gmail.com>
2025-10-06 00:19:36 +0530

    51440

--- home/models.py
+++ home/models.py
@@ -67,40 +67,58 @@ class MenuItem(models.Model):
 #     def __str__(self):
 #         return self.name
         
-class OpeningHour(models.Model):
-    day = models.CharField(max_length=20)
-    open_time = models.TimeField()
-    close_time = models.TimeField()
+# class OpeningHour(models.Model):
+#     day = models.CharField(max_length=20)
+#     open_time = models.TimeField()
+#     close_time = models.TimeField()
+
+#     # def __str__(self):
+#         return f"{self.day}: {self.open_time} - {self.close_time}"
+# class TodaySpecial(models.Model):
+#     item_name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+#     def __str__(self):
+#         return self.item_name
+# class About(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     image = models.ImageField(upload_to='about_images/', blank=True, null=True)
+
+#     def __str__(self):
+#         return self.title
+# class Feedback(models.Model):
+#     name = models.CharField(max_length=100)
+#     feedback = models.TextField()
+#     submitted_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.name} - {self.submitted_at.strftime('%Y-%m-%d')}"
+# class MenuItem(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField(blank=True)
+#     price = models.DecimalField(max_digits=8, decimal_places=2)
+#     is_available = models.BooleanField(default=True)
+#     category = models.CharField(max_length=50, blank=True)
+
+#     def __str__(self):
+#         return self.name
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return f"{self.day}: {self.open_time} - {self.close_time}"
-class TodaySpecial(models.Model):
-    item_name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+        return self.category_name
 
-    def __str__(self):
-        return self.item_name
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='about_images/', blank=True, null=True)
 
-    def __str__(self):
-        return self.title
-class Feedback(models.Model):
-    name = models.CharField(max_length=100)
-    feedback = models.TextField()
-    submitted_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.name} - {self.submitted_at.strftime('%Y-%m-%d')}"
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     is_available = models.BooleanField(default=True)
-    category = models.CharField(max_length=50, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='menu_items')
 
     def __str__(self):
         return self.name
