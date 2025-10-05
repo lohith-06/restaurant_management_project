a371e4969a0d9b57536b9a867e4b595bc4832760
lohith-06 <lohithlakkakula06@gmail.com>
2025-10-05 23:48:56 +0530

    51438

--- home/models.py
+++ home/models.py
@@ -95,3 +95,12 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.submitted_at.strftime('%Y-%m-%d')}"
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    category = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
